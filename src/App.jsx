6f893912b4455617250e6d57df430b1162f811bc
Lulitha Sanjaya Ranaweera <lsanjayaranaweera@gmail.com>
2025-10-13 23:33:40 -0400

    Move tracking page to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router'                          // Import Routes for defining <routes></routes>
 import HomePage from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'                   // Import CheckoutPage component
+import { CheckoutPage } from './pages/CheckoutPage'                   // Import CheckoutPage component/destructure
 import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -12,9 +13,7 @@ function App() {
       <Route index element={<HomePage />} />                          {/* Define route for HomePage path="/" same as index */}
       <Route path='/checkout' element={ <CheckoutPage />} />          {/* Define route for CheckoutPage path="/checkout" */} 
       <Route path='/orders' element={ <OrdersPage />} />              {/* Define route for Orders path="/orders" */}
-      
-
-      
+      <Route path='/tracking' element={ <TrackingPage/>} />           {/* Define route for Tracking path="/tracking" */}      
     </Routes>
   )
 }
