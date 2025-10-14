1d990c18e4f48d7514b2a76c2d4e305f9526cefb
Lulitha Sanjaya Ranaweera <lsanjayaranaweera@gmail.com>
2025-10-13 22:45:32 -0400

    Move checkout page into React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router'                          // Import Routes for defining <routes></routes>
 import HomePage from './pages/HomePage'
 import './App.css'
+import { CheckoutPage } from './pages/CheckoutPage'                   // Import CheckoutPage component
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />                          {/* Define route for HomePage path="/" same as index */}
-      <Route path='/checkout' element={<div>404 Not Found</div>} />   {/* Fallback route for undefined paths */}  
+      <Route path='/checkout' element={ <CheckoutPage />} />          {/* Fallback route for undefined paths */}  
       
     </Routes>
   )
