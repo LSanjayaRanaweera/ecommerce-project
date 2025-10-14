6f9c02270d80784409370255ec2cf11062af73d8
Lulitha Sanjaya Ranaweera <lsanjayaranaweera@gmail.com>
2025-10-13 23:24:45 -0400

    Move orders page to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router'                          // Import Routes for defining <routes></routes>
 import HomePage from './pages/HomePage'
-import './App.css'
 import { CheckoutPage } from './pages/CheckoutPage'                   // Import CheckoutPage component
-
+import OrdersPage from './pages/OrdersPage'
+import './App.css'
 
 function App() {
 
@@ -11,8 +11,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />                          {/* Define route for HomePage path="/" same as index */}
       <Route path='/checkout' element={ <CheckoutPage />} />          {/* Define route for CheckoutPage path="/checkout" */} 
-      <Route path='/orders' element={ < Orders />} />                 {/* Define route for Orders path="/orders" */}
-      <Route path='/tracking' element={ <Tracking/>} />               {/* Define route for Tracking path="/tracking" */}
+      <Route path='/orders' element={ <OrdersPage />} />              {/* Define route for Orders path="/orders" */}
+      
 
       
     </Routes>
